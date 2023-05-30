f3d7d31cce2880f5f485e2bb094471009813503a
Ajay <dev@ajay.app>
2023-05-30 14:39:30 -0400

    Add status endpoint

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 from utils.config import config
-from utils.redis_handler import get_async_redis_conn, wait_for_message, queue_high, queue_low
+from utils.redis_handler import get_async_redis_conn, wait_for_message, queue_high, queue_low, redis_conn
 from utils.logger import log
+from typing import Any
+from rq.worker import Worker
 
 from utils.thumbnail import generate_thumbnail, get_best_time_key, get_latest_thumbnail_from_files, get_job_id, get_thumbnail_from_files
 
@@ -88,6 +90,56 @@ async def handle_thumbnail_response(video_id: str, time: float | None, title: st
         response.headers["X-Title"] = thumbnail.title.strip()
 
     return Response(content=thumbnail.image, media_type="image/webp", headers=response.headers)
+
+@app.get("/api/v1/status")
+def get_status() -> dict[str, Any]:
+    workers = Worker.all(connection=redis_conn)
+
+    return {
+        "queues": {
+            "high": {
+                "length": len(queue_high),
+                "scheduled_jobs": queue_high.scheduled_job_registry.count,
+                "finished_jobs": queue_high.finished_job_registry.count,
+                "failed_jobs": queue_high.failed_job_registry.count,
+                "started_jobs": queue_high.started_job_registry.count,
+                "deferred_jobs": queue_high.deferred_job_registry.count,
+                "cancelled_jobs": queue_high.canceled_job_registry.count,
+            },
+            "default": {
+                "length": len(queue_low),
+                "scheduled_jobs": queue_low.scheduled_job_registry.count,
+                "finished_jobs": queue_low.finished_job_registry.count,
+                "failed_jobs": queue_low.failed_job_registry.count,
+                "started_jobs": queue_low.started_job_registry.count,
+                "deferred_jobs": queue_low.deferred_job_registry.count,
+                "cancelled_jobs": queue_low.canceled_job_registry.count,
+            },
+        },
+        "workers": [get_worker_info(worker) for worker in workers],
+        "workers_count": len(workers),
+    }
+
+def get_worker_info(worker: Worker) -> dict[str, Any]:
+    current_job = worker.get_current_job()
+    return {
+        "state": worker.state,
+        "current_job": {
+            "id": current_job.id,
+            "description": current_job.description,
+            "origin": current_job.origin,
+            "created_at": current_job.created_at,
+            "enqueued_at": current_job.enqueued_at,
+            "started_at": current_job.started_at,
+            "ended_at": current_job.ended_at,
+            "exc_info": current_job.exc_info,
+            "meta": current_job.meta,
+        } if current_job is not None else None,
+        "birth_date": worker.birth_date,
+        "successful_job_count": worker.successful_job_count,
+        "failed_job_count": worker.failed_job_count,
+        "total_working_time": worker.total_working_time,
+    }
 
 if __name__ == "__main__":
     import uvicorn
