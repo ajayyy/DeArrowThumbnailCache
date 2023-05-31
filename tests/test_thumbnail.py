89a1e994e45b1544b0d744346a3afc64f8dad960
Ajay <dev@ajay.app>
2023-05-31 02:08:20 -0400

    Add test for LRU

--- tests/test_thumbnail.py
+++ tests/test_thumbnail.py
@@ -1,12 +1,15 @@
 from multiprocessing import Process
 import os
 import shutil
+import time
+from unittest.mock import patch
 
 from fastapi import Response
 import pytest
 from rq.worker import Worker
 from app import get_thumbnail
-from utils.redis_handler import reset_async_redis_conn, redis_conn
+from utils.cleanup import cleanup, last_used_element_key, last_used_key
+from utils.redis_handler import get_async_redis_conn, reset_async_redis_conn, redis_conn
 from utils.thumbnail import generate_thumbnail, get_file_paths
 
 # Clear test cache folder
@@ -45,9 +48,9 @@ async def test_thumbnail_with_title_generate_now():
     thread = Process(target=worker.work, args=(worker,))
     thread.start()
 
-    test_video_id = "jNQXAC9IVRw"
+    test_video_id = "bdq-IYxhByw"
     test_time = 15.0
-    test_title = "Me at the zoo"
+    test_title = "Not me at the zoo"
     await load_and_verify_request(test_video_id, test_time, test_title, True, True)
 
     # Try again without sending the title
@@ -55,6 +58,31 @@ async def test_thumbnail_with_title_generate_now():
 
     thread.kill()
 
+def fake_folder_size(path: str) -> tuple[int, int]:
+    if path == "test-cache":
+        return (100001, 1)
+    else:
+        return (100, 0)
+
+@pytest.mark.asyncio
+async def test_cleanup():
+    with patch("utils.cleanup.get_folder_size", wraps=fake_folder_size):
+        new_video_id = "bdq-IYxhByw"
+        old_video_id = "jNQXAC9IVRw"
+
+        # Make this video the newest
+        await (await get_async_redis_conn()).zadd(name=last_used_key(), mapping={
+            last_used_element_key(new_video_id): int(time.time())
+        })
+
+        assert os.path.exists(os.path.join("test-cache", new_video_id))
+        assert os.path.exists(os.path.join("test-cache", old_video_id))
+
+        cleanup()
+
+        assert os.path.exists(os.path.join("test-cache", new_video_id))
+        assert not os.path.exists(os.path.join("test-cache", old_video_id))
+    
 async def load_and_verify_request(video_id: str, time: float, title: str | None = None, send_title: bool = False, generate_now: bool = False) -> None:
     test_response = Response()
     test_result = await get_thumbnail(test_response, video_id, time, generate_now, title if send_title else None)
