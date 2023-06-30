f8b1ab70dd19e369b832447c276042f7a864cfe8
Ajay <dev@ajay.app>
2023-06-30 15:44:55 -0400

    Use redis to store proxy urls

--- utils/proxy.py
+++ utils/proxy.py
@@ -1,24 +1,23 @@
+import json
 import requests
 from utils.config import config
 import time
 import random
+from utils.redis_handler import redis_conn
+from typing import Any
 
 def get_wait_period() -> int:
     return random.randint(15, 60) * 60
 
-proxies = []
-last_fetch = 0
-next_wait_period = get_wait_period()
-
-def fetch_proxies():
-    global proxies, next_wait_period, last_fetch
-
+def fetch_proxies() -> list[Any]:
     if config["proxy_token"] is None:
         raise ValueError("Proxy token not set in config.yaml")
     
+    next_wait_period = float(redis_conn.get("next_proxy_fetch") or 0)
+    last_fetch = float(redis_conn.get("last_proxy_fetch") or 0)
     if time.time() - last_fetch > next_wait_period:
-        next_wait_period = get_wait_period()
-        last_fetch = time.time()
+        redis_conn.set("next_proxy_fetch", get_wait_period())
+        redis_conn.set("last_proxy_fetch", time.time())
 
         response = requests.get(
             "https://proxy.webshare.io/api/v2/proxy/list/?mode=direct&page=1&page_size=100&ordering=-valid",
@@ -28,16 +27,21 @@ def fetch_proxies():
         result = response.json()
         if "results" in result:
             proxies = [result for result in result["results"] if result["valid"]]
+            redis_conn.set("proxies", json.dumps(proxies))
+
+            return proxies
         else:
-            # Wait at least a minute for the rate limit to clear
-            next_wait_period = random.randint(60, 90)
+            # Wait a minute for the rate limit to clear
+            redis_conn.set("next_proxy_fetch", 60)
+
+    return json.loads(redis_conn.get("proxies") or "[]")
 
 
 def get_proxy_url() -> str:
     if config["proxy_token"] is None:
         raise ValueError("Proxy token not set in config.yaml")
 
-    fetch_proxies()
+    proxies = fetch_proxies()
 
     if len(proxies) == 0:
         raise ValueError("No proxies available at the moment")
