6d8b346d551162ff59f925f67ceea09f9a89c9cc
Ajay <dev@ajay.app>
2026-08-20 01:33:24 -0400

    only web embed

--- utils/video.py
+++ utils/video.py
@@ -23,7 +23,7 @@ def create_ytdlp_object():
         "extractor_args": {
             "youtube": {
                 # "skip": ["dash", "hls", "translated_subs"],
-                "player_client": ["web_embedded", "visionos"]
+                "player_client": ["web_embedded"]
             }
         }
     })
