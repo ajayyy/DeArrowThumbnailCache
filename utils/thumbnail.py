1e80b8de412102f3c8647241d1913c124544e6c7
Ajay <dev@ajay.app>
2026-08-21 11:42:34 -0400

    Remove sleep again

--- utils/thumbnail.py
+++ utils/thumbnail.py
@@ -127,7 +127,7 @@ def generate_and_store_thumbnail(video_id: str, time: float, is_livestream: bool
 def generate_with_ffmpeg(video_id: str, time: float, playback_url: PlaybackUrl,
                             is_livestream: bool, proxy_url: str | None = None) -> None:
     wait_time = 0
-    time_module.sleep(5)
+    # time_module.sleep(5)
     while redis_conn.zcard("concurrent_renders") > config["max_concurrent_renders"]:
         print("Waiting for other renders to finish")
         wait_time += 1
